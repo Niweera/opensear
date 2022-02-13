a4da5af83f4d9df20d56aaaa6c49e1d4181e8885
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-02-13 19:08:10 +0530

    Update ESLint rules

--- opensearobot/index.test.js
+++ opensearobot/index.test.js
@@ -29,4 +29,31 @@ const headless = async () => {
   }
 };
 
-headless().then();
+const headfull = async () => {
+  try {
+    const tweetID = "1492803981021945856";
+    const openSeaRobot = new OpenSeaRobot();
+    const twitterService = new TwitterService();
+    const metadata = await twitterService.getMetadata(tweetID);
+    metadata.name = "wordle osr test #" + new Date().getTime();
+
+    const __dirname = path.dirname(
+        path.dirname(fileURLToPath(import.meta.url))
+    );
+    const imageFilePath = path.resolve(
+        path.join(__dirname, "temp", `${tweetID}.png`)
+    );
+
+    const assetURL = await openSeaRobot.run(imageFilePath, metadata);
+    await twitterService.addReplyToTweet(tweetID, assetURL);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+if (process.platform !== "win32"){
+  headless().then();
+}else{
+  headfull().then();
+}
+
