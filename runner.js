e8206227076a8140e007620c361ca87ea5d63ca2
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-02-14 02:58:19 +0530

    Update runner script

--- runner.js
+++ runner.js
@@ -1,4 +1,3 @@
-import Queue from "bull";
 import config from "./keys";
 import Xvfb from "xvfb";
 import TweetPikService from "./services/tweetpik-service";
@@ -8,60 +7,45 @@ import TwitterService from "./services/twitter-service";
 
 function start() {
   console.log("worker dispatching...");
-  const workQueue = new Queue(config.REDIS_QUEUE_NAME, config.REDIS_URL);
-
-  workQueue.process(
-    config.BULL_PROCESS_NAME,
-    config.MAX_JOBS_PER_WORKER,
-    async (job, done) => {
-      try {
-        job.progress(0);
-        const tweetID = job.id;
-        // console.log(`Now executing job ${tweetID}`);
-        // const tweetPikService = new TweetPikService();
-        // const imageService = new ImageService();
-        // const twitterService = new TwitterService();
-        //
-        // const originalScreenshot = await tweetPikService.getScreenshot(tweetID);
-        job.progress(25);
-        //
-        // const imageFilePath = await imageService.removeWatermark(
-        //   originalScreenshot,
-        //   tweetID
-        // );
-        job.progress(50);
-        //
-        // const metadata = await twitterService.getMetadata(tweetID);
-        job.progress(75);
-        //
-        // let assetURL;
-        //
-        if (process.platform !== "win32") {
-          const xvfb = new Xvfb();
-          xvfb.startSync();
-          console.log("xvfb works");
-          //   const openSeaRobot = new OpenSeaRobot();
-          //   assetURL = await openSeaRobot.run(imageFilePath, metadata);
-          xvfb.stopSync();
-        } else {
-          console.log("platform is windows??????");
-          //   const openSeaRobot = new OpenSeaRobot();
-          //   assetURL = await openSeaRobot.run(imageFilePath, metadata);
-        }
-        //
-        // await twitterService.addReplyToTweet(tweetID, assetURL);
-        //
-        job.progress(100);
-        console.log(`Job ${tweetID} executed`);
-        done();
-        process.exit(0);
-      } catch (e) {
-        console.error(`Job failed with error: ${e.message}`);
-        done(new Error(e));
-        process.exit(1);
-      }
+  try {
+    const tweetID = "1492597351089045505";
+    // console.log(`Now executing job ${tweetID}`);
+    // const tweetPikService = new TweetPikService();
+    // const imageService = new ImageService();
+    // const twitterService = new TwitterService();
+    //
+    // const originalScreenshot = await tweetPikService.getScreenshot(tweetID);
+    //
+    // const imageFilePath = await imageService.removeWatermark(
+    //   originalScreenshot,
+    //   tweetID
+    // );
+    //
+    // const metadata = await twitterService.getMetadata(tweetID);
+    //
+    // let assetURL;
+    //
+    if (process.platform !== "win32") {
+      const xvfb = new Xvfb();
+      xvfb.startSync();
+      console.log("xvfb works");
+      //   const openSeaRobot = new OpenSeaRobot();
+      //   assetURL = await openSeaRobot.run(imageFilePath, metadata);
+      xvfb.stopSync();
+    } else {
+      console.log("platform is windows??????");
+      //   const openSeaRobot = new OpenSeaRobot();
+      //   assetURL = await openSeaRobot.run(imageFilePath, metadata);
     }
-  );
+    //
+    // await twitterService.addReplyToTweet(tweetID, assetURL);
+    //
+    console.log(`Job ${tweetID} executed`);
+    process.exit(0);
+  } catch (e) {
+    console.error(`Job failed with error: ${e.message}`);
+    process.exit(1);
+  }
 }
 
 start();
